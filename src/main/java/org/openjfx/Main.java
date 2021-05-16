6aee201d17d2188073b2cdfaa62f4632d4a02742
ancaavram22 <anca_avram@yahoo.com>
2021-05-16 22:56:19 +0300

    slogans for starting menu pane

--- src/main/java/org/openjfx/Main.java
+++ src/main/java/org/openjfx/Main.java
@@ -8,7 +8,8 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Main extends Application {
+public class
+Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
